acac5f1ab85a946c5ee4ee88d4c5322161a85075
Glacian22 <47679905+Glacian22@users.noreply.github.com>
2019-09-09 14:49:23 -0700

    set up mocha/chai testing

---
+++ test/sum.test.js
@@ -0,0 +1,13 @@
+const sum = require('../sum');
+var should = require('chai').should;
+var expect = require('chai').expect;
+
+describe('tests for blobber', function () {
+  it('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).to.equal(3);
+  });
+
+  it('adds 2 and 2 to be greater than 3', () => {
+    expect(sum(2, 2)).to.be.above(3)
+  });
+});
